b7e8bd67c45c07b3305a334d9ff6a85628c4514f
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-09-26 21:45:25 +0500

    feat: add tusk

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minimum = null;
+        this.maximum = null;
+        this.guessingRange = null;
+    }
 
     setRange(min, max) {
-
+        this.maximum = max;
+        this.minimum = min;
     }
 
     guess() {
-
+        this.guessingRange = Math.ceil((this.maximum + this.minimum)/2);
+        return this.guessingRange;
     }
 
     lower() {
-
+        this.maximum = this.guessingRange;
     }
 
     greater() {
-
+        this.minimum = this.guessingRange;
     }
 }
 
